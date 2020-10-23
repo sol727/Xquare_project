1f9fd008e17f154bfc4e36db003a71af54ce0b77
ajh2@hubdnc.com <ajh@hubdnc.com>
2020-10-23 19:01:33 +0900

    201023 프로젝트gnb추가중

--- src/pages/v3_project_state.js
+++ src/pages/v3_project_state.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Studio_gnb from '../components/Studio_gnb'
 
 export default class V3_Project_state extends React.Component {
 
@@ -9,7 +10,9 @@ export default class V3_Project_state extends React.Component {
     render() {
         return (
             <>
-            
+               <section className="v3 studio state">
+                    <Studio_gnb />
+               </section>
             </>
         )
     }
