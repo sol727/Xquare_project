f54a052d445fb10431dbbc46d2dd223ece7a6cfe
안재현 <tddj01015@naver.com>
2020-10-23 19:01:43 +0900

    개설한프로젝트 진행시작

--- src/pages/v3_project.js
+++ src/pages/v3_project.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Header from '../components/header'
+import Footer from '../components/footer'
+
 
 export default class V3_Project extends React.Component {
 
@@ -9,7 +12,45 @@ export default class V3_Project extends React.Component {
     render() {
         return (
             <>
-            
+                <Header />
+                <section className="v3 project">
+                    <div className="top_box container">
+                        <div className="clearfix2">
+                            <h1>개설한 프로젝트</h1>
+                            <ul className="clearfix floatR">
+                                <li>
+                                    <h4 className="playF">2</h4>
+                                    <p className="gray2_color">준비중</p>
+                                </li>
+                                <li>
+                                    <h4 className="playF">1</h4>
+                                    <p className="gray2_color">오픈예정</p>
+                                </li>
+                                <li>
+                                    <h4 className="playF">1</h4>
+                                    <p className="gray2_color">진행중</p>
+                                </li>
+                                <li>
+                                    <h4 className="playF">2</h4>
+                                    <p className="gray2_color">종료됨</p>
+                                </li>
+                                <li>
+                                    <h4 className="playF">6</h4>
+                                    <p className="gray2_color">전체</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <ul className="tab_ul clearfix2">
+                            <li>
+                                <a href="javascript:;">전체</a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">종료된 프로젝트</a>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+                <Footer />
             </>
         )
     }
