41708e723ef0cf2c243ca0dff023494a044e6bec
ajh2@hubdnc.com <ajh@hubdnc.com>
2020-10-26 21:35:31 +0900

    account page 추가

---
+++ src/pages/v3_account4.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class V3_Account4 extends React.Component {
+
+    constructor(props) {
+    super(props);
+    }
+
+    render() {
+        return (
+            <>
+            
+            </>
+        )
+    }
+}
