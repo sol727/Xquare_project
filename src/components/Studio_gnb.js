399757ead92c519e65b67d348e8bac6953d6947b
ajh2@hubdnc.com <ajh@hubdnc.com>
2020-10-26 20:31:41 +0900

    201026 계약준비 페이지 진행중

--- src/components/Studio_gnb.js
+++ src/components/Studio_gnb.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 export default class Studio_gnb extends React.Component {
 
     constructor(props) {
@@ -9,17 +10,88 @@ export default class Studio_gnb extends React.Component {
         return (
             <div className="v3 studio_gnb">
                <button className="gnb_btn"><img src={ require('../images/icons/ic_menu_left.png') } alt="gnb접기아이콘"/></button>
-               <div className="gnb_wrap">
-                    <div>
-                         
+                <div className="gnb_wrap">
+                    <div className="centerT menu_btn">
+                        <button type="button" className="gnb_btn2"></button>
                     </div>
-                    <ul>
-                         <li>
-
+                    <div className="top centerT">
+                        <img src={ require('../images/img_logo_xquare.png') } alt="logo"/>
+                        <h1>프로젝트 스튜디오</h1>
+                    </div>
+                    <div className="state_box_wrap"> 
+                        <div className="state_box">
+                            <div className="clearfix2">
+                                <p className="gray2_color font_13">준비중</p>
+                                <p className="gray2_color font_13">yymmdd-hhnn</p>
+                            </div>
+                        </div>
+                    </div>
+                    <ul className="pc">
+                         <li className="on">
+                            <Link to="/V3_Project_state">프로젝트상세</Link>
+                         </li>
+                         <li className="next">
+                            <Link to="/V3_Project_state">계약준비</Link>
+                         </li>
+                         <li className="sub_menu">
+                            <Link to="/V3_Project_state" className="arrow">자격확인</Link>
+                        </li>
+                        <li className="sub_menu">
+                            <Link to="/V3_Project_state" className="arrow">기본자료</Link>
+                        </li>
+                        <li className="sub_menu">
+                            <Link to="/V3_Project_state" className="arrow">발행정보</Link>
+                        </li>
+                        <li className="sub_menu">
+                            <Link to="/V3_Project_state" className="arrow">기타정보</Link>
+                        </li>
+                        <li className="">
+                            <Link to="/V3_Project_state">펀딩준비</Link>
+                         </li>
+                         <li className="sub_menu">
+                            <Link to="/V3_Project_state" className="arrow">발행정보 확정</Link>
+                        </li>
+                        <li className="sub_menu">
+                            <Link to="/V3_Project_state" className="arrow">펀딩 소개 작성</Link>
+                        </li>
+                        <li className="sub_menu">
+                            <Link to="/V3_Project_state" className="arrow">추가 자료</Link>
+                        </li>
+                        <li className="">
+                            <Link to="/V3_Project_state" className="arrow">엑스퀘어 피드백 <span className="count">9999</span></Link>
+                         </li>
+                         <li className="">
+                            <Link to="/V3_Project_state" className="arrow">투자자 현황 </Link>
+                         </li>
+                         <li className="">
+                            <Link to="/V3_Project_state" className="arrow">투자자 의견 <span className="count">1</span></Link>
+                         </li>
+                         <li className="alert">
+                            <Link to="/V3_Project_state" className="arrow">새소식 관리  <img src={ require('../images/icons/ic_ic_alert_alert_circle.svg') } alt="느낌표아이콘"/></Link>
+                         </li>
+                         <li className="alert">
+                            <Link to="/V3_Project_state" className="arrow">결산자료 제출 및 확인  <img src={ require('../images/icons/ic_ic_alert_alert_circle.svg') } alt="느낌표아이콘"/></Link>
+                         </li>
+                         <li className="">
+                            <Link to="/V3_Project_state" className="arrow">투자광고 </Link>
+                         </li>
+                         <li className="">
+                            <Link to="/V3_Project_state" className="arrow">설정 </Link>
+                         </li>
+                         <li className="">
+                            <Link to="/V3_Project_state" className="arrow">프로젝트 문의</Link>
                          </li>
                     </ul>
-               </div>
-               
+                    <ul className="mo">
+                        <li>
+                            <a href=""></a>
+                            <p>프로젝트상세</p>
+                        </li>
+                    </ul>
+                    <div className="bottom rightT"> 
+                        <button type="button" className="studio_btn">프로젝트 삭제</button>
+                    </div>
+               </div>               
             </div>
         )
     }
---
+++ src/components/radio.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import $ from 'jquery'
+export default class Radio extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount(){
+
+     
+          // var chk_radio = document.querySelector('input[type="radio"]');
+          // var parent = checked.parentNode; 
+		// var sel_type = null;
+		// for(var i=0;i<chk_radio.length;i++){
+		// 	if(chk_radio[i].checked == true){ 
+		// 		sel_type = chk_radio[i].value;
+		// 	}
+		// }
+		// if(sel_type == null){
+
+          //      parent.classList.add('on');
+		// 	return false;
+		// }else{
+          //      parent.classList.remove('on');
+          // }
+          
+     
+    
+
+
+
+     
+     // var checked= document.querySelector('input[type="radio"]');
+     // var parent = checked.parentNode; 
+     //      if(checked.checked == true){ 
+     //           parent.classList.add('on');
+     //      }else{
+     //           parent.classList.remove('on');
+     //      } 
+    }
+    
+
+    render() {
+        return (
+          <div className="radio_box">
+               <input type="radio" name="" className={`q w ${this.props.class}`} 
+               id={this.props.id}/><label htmlFor={this.props.id}></label><label htmlFor={this.props.id}>{this.props.text}</label>
+          </div>
+        )
+    }
+}
