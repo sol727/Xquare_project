fc6cf518fec371e68bc96f0b0122c34c9a99bfdb
안재현 <tddj01015@naver.com>
2020-10-28 22:21:54 +0900

    결산자료제출 진행중 80%

--- src/pages/v3_data.js
+++ src/pages/v3_data.js
@@ -45,7 +45,7 @@ export default class V3_data extends React.Component {
                                     <div class="notice_box">
                                         <h3 class="border_text font_16">해당 프로젝트의 주식을 소각했거나 채권 상환을 완료했거나 사업보고서 제출의 대상이신가요?</h3>
                                     </div>
-                                    <div className="success_li">
+                                    <div className="success_li info_box">
                                         <ProjectContent 
                                             state="성공" 
                                             date="200701-1301" 
@@ -83,35 +83,173 @@ export default class V3_data extends React.Component {
                                                 <p className="font_13 gray2_color">·주주총회 결의 후 법인인감이 날인되어 있어야 합니다.</p>
                                             </div>
                                         </div>
-                                        <div className="bd_box bd_none clearfix2">
+                                        <div className="bd_box bd_none clearfix2 mb10">
                                             <div className="left">
                                                 <Title 
-                                                    num="01."
+                                                    num=""
                                                     title ="법인등기부등본"
                                                     star ="*"
                                                     
                                                     text = {
-                                                         <>
-                                                         <p>·말소사항이 포함된, 발급 1개월 이내의 법인등기부등본을 pdf, jpg,jpeg, png  파일로 업로드 해주세요.</p>
-                                                         <p className="error">·error here</p>
-                                                         </>
+                                                        <>
+                                                        <p>·아래의 사항중 해당되는 서류를 제출하세요.</p>
+                                                        <p>- 주식소삭 : 주식소각 완료를 입증하는 발행인 공문</p>
+                                                        <p>- 채권상환 : 채권상환 완료를 입증하는 발행인 공문 또는 발행등록사실확인서(전자등록 채권인 경우)</p>
+                                                        <p>- 사업보고서 제출대상 : 사업보고서 제출 대상임을 입증하는 발행인 공문 또는 상장증명서(거래소 발급)</p>
+                                                        <p className="error">·등록 할 수 없는 파일형식입니다.</p>
+                                                        </>
+                                                    }
+                                                    label_title="공시 의무 종료 증빙서류"
+                                                    labelShow={true}
+                                                    labelDisabled={false}
+                                                />
+                                            </div>
+                                            <div className="right file_box file_box2">
+                                                <div className="delt_btn_none mb16">
+                                                    <File_upload
+                                                        flie_id = "file1"
+                                                        downShow={false}
+                                                    />
+                                                </div>
+                                                <ul>
+                                                    <li className="upload_btn_none mb16">
+                                                        <File_upload
+                                                            flie_id = "file1"
+                                                            downShow={false}
+                                                        />
+                                                    </li>
+                                                    <li className="upload_btn_none mb16">
+                                                        <File_upload
+                                                            flie_id = "file1"
+                                                            downShow={false}
+                                                        />
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div className="bd_box bd_none clearfix2">
+                                            <div className="left mt16">
+                                                <Title 
+                                                    num=""
+                                                    title ="새소식 알림"
+                                                    star ="*"
+                                                    
+                                                    text = {
+                                                        <>
+                                                        <p>·이 내용은 ‘새소식'에 업로드 되며, 공시 의무 종료에 따라 투자자에게 전달할 메시지를 작성해 주시면 됩니다.</p>
+                                                        <p className="error">·error here</p>
+                                                        </>
                                                     }
                                                     label_title="투자자 노출"
                                                     labelShow={true}
                                                     labelDisabled={false}
                                                 />
                                             </div>
-                                            <div className="right file_box file_box2">
-                                                <File_upload
-                                                        flie_id = "file1"
-                                                       downShow={false}
-                                                />
+                                            <div className="right file_box2 mt16">
+                                                <div className="write_box mt24">
+                                                    <div className="title_input mb16">
+                                                        <input type="text" placeholder="제목 (최대 60자)" maxLength="60"/>
+                                                        <p>(0/60)</p>
+                                                    </div>
+                                                    <textarea name="" id="" cols="30" rows="10" placeholder="내용을 입력해주세요"></textarea>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
+                                    
+                                    {/* 결산자료제출 해당할때 */}
+                                    <div className="bottom_cont mt10">
+                                        <div className="top_wrap">
+                                            <p className="font_14">이 프로젝트는</p>
+                                            <div class="notice_box mt8 mb24">
+                                                <h3 class="border_text font_24">결산자료 게재 의무 대상입니다</h3>
+                                            </div>
+                                            <p className="font_13 gray2_color line_h5">자본시장법시행령 제118조16 제3항 제2호에 따라 발행인은 매 사업연도 경과 후 90일 이내에 재무제표 등 결산 관련 서류를 중개업자 홈페이지에 게재해야 합니다.</p>
+                                            <div className="gray_box">
+                                                <p className="font_13 gray2_color mb10">·pdf파일로 업로드 해주시기 바랍니다.</p>
+                                                <p className="font_13 gray2_color">·주주총회 결의 후 법인인감이 날인되어 있어야 합니다.</p>
+                                            </div>
+                                        </div>
+                                        <div className="bd_box bd_none clearfix2 mb10">
+                                            <div className="left">
+                                                <Title 
+                                                    num=""
+                                                    title ="법인등기부등본"
+                                                    star ="*"
+                                                    
+                                                    text = {
+                                                        <>
+                                                        <p>·아래의 사항중 해당되는 서류를 제출하세요.</p>
+                                                        <p>- 주식소삭 : 주식소각 완료를 입증하는 발행인 공문</p>
+                                                        <p>- 채권상환 : 채권상환 완료를 입증하는 발행인 공문 또는 발행등록사실확인서(전자등록 채권인 경우)</p>
+                                                        <p>- 사업보고서 제출대상 : 사업보고서 제출 대상임을 입증하는 발행인 공문 또는 상장증명서(거래소 발급)</p>
+                                                        <p className="error">·등록 할 수 없는 파일형식입니다.</p>
+                                                        </>
+                                                    }
+                                                    label_title="공시 의무 종료 증빙서류"
+                                                    labelShow={true}
+                                                    labelDisabled={false}
+                                                />
+                                            </div>
+                                            <div className="right file_box file_box2">
+                                                <div className="delt_btn_none mb16">
+                                                    <File_upload
+                                                        flie_id = "file1"
+                                                        downShow={false}
+                                                    />
+                                                </div>
+                                                <ul>
+                                                    <li className="upload_btn_none mb16">
+                                                        <File_upload
+                                                            flie_id = "file1"
+                                                            downShow={false}
+                                                        />
+                                                    </li>
+                                                    <li className="upload_btn_none mb16">
+                                                        <File_upload
+                                                            flie_id = "file1"
+                                                            downShow={false}
+                                                        />
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div className="bd_box bd_none clearfix2">
+                                            <div className="left mt16">
+                                                <Title 
+                                                    num=""
+                                                    title ="새소식 알림"
+                                                    star ="*"
+                                                    
+                                                    text = {
+                                                        <>
+                                                        <p>·이 내용은 ‘새소식'에 업로드 되며, 공시 의무 종료에 따라 투자자에게 전달할 메시지를 작성해 주시면 됩니다.</p>
+                                                        <p className="error">·error here</p>
+                                                        </>
+                                                    }
+                                                    label_title="투자자 노출"
+                                                    labelShow={true}
+                                                    labelDisabled={false}
+                                                />
+                                            </div>
+                                            <div className="right file_box2 mt16">
+                                                <div className="write_box mt24">
+                                                    <div className="title_input mb16">
+                                                        <input type="text" placeholder="제목 (최대 60자)" maxLength="60"/>
+                                                        <p>(0/60)</p>
+                                                    </div>
+                                                    <textarea name="" id="" cols="30" rows="10" placeholder="내용을 입력해주세요"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="btn_wrap rightT mt10">
+                                        <button type="button" className="orange_btn mr24">임시저장</button>
+                                        {/* 제출버튼 활성화시 클래스에 orange_btn 추가 */}
+                                        <button type="button" className="unable_btn ">제출</button>
+                                    </div>
                                 </div>
-
-
                             </div>
                         </div>
                     </div>
